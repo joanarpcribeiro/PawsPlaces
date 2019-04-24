5646af77415b497cde8d85cbf9585de9796696d8
Doris <dorisc.santos@gmail.com>
2019-04-24 15:12:38 +0100

    l

--- pawsPlaces/routes/index.js
+++ pawsPlaces/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Place = require("../models/Place");
-const User = require('../models/User');
+const User = require('../models/User'); 
 const { checkConnected, checkAdmin, checkRole } = require('../middlewares')
 
 /* GET home page */
